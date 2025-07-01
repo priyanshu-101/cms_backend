70f7afb88393d1c4b8ef3874e8446d1f471ea894
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-01 08:24:16 +0530

    student model

---
+++ models/Student.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    grade: {
+        type: String,
+        required: true,
+    },
+    batchIds: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Batch',
+    },
+    enrollmentDate: {
+        type: Date,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
